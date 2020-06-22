afefa4e11588639c16376ab0a2d822793d68487c
lokentran <thanhtungmarine@gmail.com>
2020-06-22 23:20:23 +0700

    Add element to array

---
+++ array_push.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+    $a = ['red', 'green'];
+    array_push($a, 'blue', 'yellow');
+
+    echo "<pre>";
+    print_r($a);
+    echo "</pre>";
+
+?>
+
+
+</body>
+</html>
