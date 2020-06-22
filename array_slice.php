1658c08592162dbd1add728a28f799fddb90e95a
lokentran <thanhtungmarine@gmail.com>
2020-06-22 23:43:24 +0700

    user array_slice

---
+++ array_slice.php
@@ -0,0 +1,14 @@
+<?php
+// Tra ve cac phan tu duoc chon cua mang
+
+$a=["red","green","blue","yellow","brown"];
+echo "<pre>";
+print_r(array_slice($a,1,3));
+echo "</pre>";
+
+echo "<pre>";
+print_r($a);
+echo "</pre>";
+
+
+?>
